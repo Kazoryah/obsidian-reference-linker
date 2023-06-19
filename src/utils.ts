039be741f0f25b68620562777f85240d86a17f74
Romain <romain.hermary@uni.lu>
2023-06-19 14:15:57 +0200

    annotations h2

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Annotation } from './pdf/PDFManager'
 
 export function formatAnnotations(annotations: Annotation[]) : string {
-    let render = "\n\n# Annotations\n\n";
+    let render = "\n\n## Annotations\n\n";
     for (const annotation of annotations) {
         render += `<mark style="background: ${annotation.highlight.color};">`;
         render += `${annotation.highlight.text}</mark>`;
